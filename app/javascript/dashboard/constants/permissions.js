3fac8b14deeb89a0a7c37b6bf043c0f40174bb2e
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2026-01-14 16:21:00 -0300

    fix mostrar aba todos

--- app/javascript/dashboard/constants/permissions.js
+++ app/javascript/dashboard/constants/permissions.js
@@ -45,7 +45,7 @@ export const ASSIGNEE_TYPE_TAB_PERMISSIONS = {
   all: {
     count: 'allCount',
     permissions: [
-      ROLES['administrator'],
+      'administrator',
       MANAGE_ALL_CONVERSATION_PERMISSIONS,
       CONVERSATION_PARTICIPATING_PERMISSIONS,
     ],
